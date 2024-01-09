67868ab451915c45fa595f0740a55680b742d1fd
gabriel <dgabrielfof@gmail.com>
2024-01-09 14:27:51 -0300

    :card_file_box: raw: types of inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,59 @@
-const state = {
+import { registerBox } from "./pages/register.js";
+
+export const state = {
   user: {
 
   },
   view: {
     container: document.querySelector(".container"),
   },
+  values: {
+    inputs: {
+      name: {
+        name: "nome",
+        key: "name",
+        type: "text",
+        pages: ["signup"],
+        value: null,
+      },
+      email: {
+        name: "e-mail",
+        key: "e-mail",
+        type: "text",
+        pages: ["signin", "signup"],
+        value: null,
+      },
+      password: {
+        name: "senha",
+        key: "password",
+        type: "password",
+        pages: ["signin", "signup"],
+        value: null,
+      },
+      confirmPassword: {
+        name: "confirmar senha",
+        key: "confirm-password",
+        type: "password",
+        pages: ["signup"],
+        value: null,
+      },
+    }
+  }
+}
+
+export function cleanOrCreateBox(className) {
+  let box = document.querySelector(`.${className}`);
+  if (box) {
+    box.innerHTML = "";
+  } else {
+    box = document.createElement("div");
+    box.classList.add(className);
+  }
+  return box;
 }
 
 function init() {
+  registerBox()
 }
 
 init();
