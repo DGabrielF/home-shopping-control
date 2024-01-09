747dc9dd2a30719c98b380b47ef3fcbe45fd551d
gabriel <dgabrielfof@gmail.com>
2024-01-09 18:10:58 -0300

    feat: adding the scripts to the registration page

--- src/scripts/pages/register.js
+++ src/scripts/pages/register.js
@@ -1,5 +1,17 @@
 import { separator } from "../components/separator.js";
+import { removeToast, toast } from "../components/toast.js";
 import { cleanOrCreateBox, state } from "../main.js";
+import { emailValidation, nameValidation, passwordValidation } from "../validations.js";
+
+const localState = {
+  values: {
+    name: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  },
+  errors : [],
+}
 
 export function registerBox () {
   const container = cleanOrCreateBox("container");
@@ -17,41 +29,102 @@ export function registerBox () {
   centralContainer.appendChild(entriesArea);
   
   centralContainer.appendChild(separator());
-
+  
   const buttonArea = createButtonsArea();
   centralContainer.appendChild(buttonArea);
-
+  
   container.appendChild(centralContainer);  
+  localState.errors = handleValidation()
+}
+
+function createEntriesArea() {
+  const entriesArea = document.createElement("div");
+  entriesArea.classList.add("entries-area");
+  
+  for (const key in state.values.inputs) {
+    if (state.values.inputs[key].pages.includes("signup")) {
+      const label = document.createElement("label");
+      label.textContent = state.values.inputs[key].name;
+      entriesArea.appendChild(label);
+      
+      const input = document.createElement("input");
+      input.id = `${state.values.inputs[key].key}-input`;
+      input.type = state.values.inputs[key].type;
+      input.addEventListener("input", (e) => {
+        localState.values[key] = e.target.value;
+        localState.errors = handleValidation();
+        if (localState.errors.length > 0) {
+          toast(localState.errors.length>1?"Erros":"Erro", "error", localState.errors);
+          const confirmButton = document.querySelector(".confirm");
+          confirmButton.disabled = true;
+        } else {
+          removeToast();
+          const confirmButton = document.querySelector(".confirm");
+          confirmButton.disabled = false;
+          
+        }
+      })
+      entriesArea.appendChild(input);
+    }
+  }
+  return entriesArea;
 }
 
 function createButtonsArea() {
   const buttonArea = document.createElement("div");
-  buttonArea.classList.add("button-area");
+  buttonArea.classList.add("buttons-area");
 
   const confirm = document.createElement("button");
+  confirm.disabled = true
   confirm.textContent = "confirmar";
+  confirm.addEventListener("click", () => {
+    // Registrar no banco de dados
+    toast("Cadastro realizado", "success", "Entre pela primeira vez");
+    setTimeout(() => removeToast(), 3000);
+  })
   confirm.classList.add("confirm");
   buttonArea.appendChild(confirm);
-
+  
   const login = document.createElement("button");
   login.textContent = "sou cadastrado";
   buttonArea.appendChild(login);
   return buttonArea;
 }
 
-function createEntriesArea() {
-  const entriesArea = document.createElement("div");
-  entriesArea.classList.add("entries-area");
-
-  for (const key in state.values.inputs) {
-    const label = document.createElement("label");
-    label.textContent = state.values.inputs[key].name;
-    entriesArea.appendChild(label);
-
-    const input = document.createElement("input");
-    input.id = `${state.values.inputs[key].key}-input`;
-    input.type = state.values.inputs[key].type;
-    entriesArea.appendChild(input);
+function handleValidation() {
+  const errors = [];
+  
+  const nameInput = document.querySelector("#name-input");
+  const isNameOk = nameValidation(localState.values.name, state.values.settings.name, errors)
+  if (isNameOk) {
+    nameInput.classList.remove("error");
+  } else {
+    nameInput.classList.add("error");
+  };
+  
+  const emailInput = document.querySelector("#e-mail-input");
+  if (!emailValidation(localState.values.email)) {
+    errors.push("E-mail inválido");
+    emailInput.classList.add("error");
+  } else {
+    emailInput.classList.remove("error");
   }
-  return entriesArea;
+  // TODO Verificar se o e-mail já está cadastrado
+
+  const passwordInput = document.querySelector("#password-input");
+  const isPasswordOk = passwordValidation(localState.values.password, state.values.settings.password, errors);
+  if (isPasswordOk) {
+    passwordInput.classList.remove("error");
+  } else {
+    passwordInput.classList.add("error");
+  }
+
+  const confirmInput = document.querySelector("#confirm-password-input");
+  if (localState.values.password !== localState.values.confirmPassword) {
+    errors.push("As senhas precisam ser iguais");   
+    confirmInput.classList.add("error"); 
+  } else {
+    confirmInput.classList.remove("error")
+  }
+  return errors;
 }
